9db88685a06460327b3e4a1d19247124e9cc92a6
Angiefm <126833221+Angiefm@users.noreply.github.com>
2023-04-28 13:16:59 -0500

    First-Class-Homework

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,7 +1,7 @@
 import pygame
 
 from pygame.sprite import Sprite
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 
 class Dinosaur(Sprite):
@@ -17,22 +17,36 @@ class Dinosaur(Sprite):
         self.jump_speed = self.JUMP_SPEED
         self.dino_run = True
         self.dino_jump = False
+        self.dino_duck = False
         self.step_index = 0
     
     def update(self, user_input):
-        if self.dino_run:
-            self.run()
+        if self.dino_run:   # si el estdo del dino es run 
+            self.run()      # llama a self.run()
         elif self.dino_jump:
             self.jump()
+        elif self.dino_duck:
+            self.duck()
+        
 
-        if user_input[pygame.K_UP] and not self.dino_jump:
+        if user_input[pygame.K_UP] and not self.dino_jump: ##el and not self.dino_jump es para que si la tecla se presiona de nuevo no se haga un nuevo salto
             self.dino_jump = True
+            self.dino_duck = False
             self.dino_run = False
         elif not self.dino_jump:
             self.dino_run = True
+        
+        if user_input[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_jump : False
+            self.dino_run = False
+            self.dino_duck = True
+        elif not self.dino_duck:
+            self.dino_duck : False
+            self.dino_jump : False
+            self.dino_run : True
 
-        if self.step_index >=10:
-            self.stop_index = 0
+        if self.step_index >=10: # para intercalar.
+            self.step_index = 0
 
     def run(self):
         self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
@@ -43,13 +57,21 @@ class Dinosaur(Sprite):
 
     def jump(self):
         self.image = JUMPING 
-        self.dino_rect.y -= self.jump_speed * 4
+        self.dino_rect.y -= self.jump_speed * 4 # tarda cuatro corridas para llegar a la posición de arriba y luego baja
         self.jump_speed -= 0.8
 
         if self.jump_speed < -self.JUMP_SPEED:
             self.dino_rect.y = self.Y_POS
             self.jump_speed = self.JUMP_SPEED
             self.dino_jump =False
+
+    def duck(self):
+        self.image=DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.x_POS
+        self.dino_rect.y = self.Y_POS + 35
+        self.dino_duck = False
+
 
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
